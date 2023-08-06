611eb199b45d3d26c0ca8caf520b68e94472793a
Federico Abrignani <federignoli@hotmail.it>
2023-08-06 12:14:38 +0200

    Add DeterministicConcurrency lib

---
+++ cmake/DeterministicConcurrency.cmake
@@ -0,0 +1,9 @@
+include(FetchContent)
+
+FetchContent_Declare(
+  dc
+  GIT_REPOSITORY https://github.com/Sernior/deterministic-concurrency.git
+  GIT_TAG v1.0.0
+)
+
+FetchContent_MakeAvailable(dc)
