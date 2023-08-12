d4ddcc55abf9a06eb70fa862d83ee8d07024310b
Federico Abrignani <federignoli@hotmail.it>
2023-08-12 19:05:04 +0200

    Update priority_shared_mutex
    Change priority_shared_mutex priorities to use only 1 condition variable and 1 counter.

--- include/priority_shared_mutex.h
+++ include/priority_shared_mutex.h
@@ -11,15 +11,12 @@ namespace PrioSync{// the name has yet to be chosen
     template<Priority_t N = 1, typename = std::enable_if_t<(N >= 1 && N <= _max_priority)>>
     class priority_shared_mutex{
 
-        using Reader_cnt_t = uint16_t;
-        using Writer_cnt_t = uint16_t;
-        static constexpr Reader_cnt_t _Max_readers = Reader_cnt_t(-1);
+        using Thread_cnt_t = uint32_t;
+        static constexpr Thread_cnt_t _max_threads = Thread_cnt_t(-1);
 
         struct threadPriority{
-            Writer_cnt_t writers_waiting{};
-            std::condition_variable writer_queue;
-            Reader_cnt_t readers_waiting{};
-            std::condition_variable reader_queue;
+            Thread_cnt_t threads_waiting{};
+            std::condition_variable thread_queue;
         };
 
         public:
@@ -43,9 +40,9 @@ namespace PrioSync{// the name has yet to be chosen
             auto& myPriority = _priorities[priority];
 
             while (_lockOwned || _totalCurrentReaders > 0 || _find_first_priority(priority) < priority ){ 
-                myPriority.writers_waiting++;
-                myPriority.writer_queue.wait(lock);
-                myPriority.writers_waiting--;
+                myPriority.threads_waiting++;
+                myPriority.thread_queue.wait(lock);
+                myPriority.threads_waiting--;
             }
 
             _lockOwned = true;
@@ -61,8 +58,7 @@ namespace PrioSync{// the name has yet to be chosen
             }
 
             if (p != _max_priority){
-                _priorities[p].writer_queue.notify_one();
-                _priorities[p].reader_queue.notify_all();
+                _priorities[p].thread_queue.notify_all();
             }
 
         }
@@ -87,10 +83,10 @@ namespace PrioSync{// the name has yet to be chosen
             std::unique_lock<std::mutex> lock(_internalMtx);
             auto& myPriority = _priorities[priority];
 
-            while (_lockOwned || _totalCurrentReaders == _Max_readers || _find_first_priority(priority) < priority ){ 
-                myPriority.readers_waiting++;
-                myPriority.reader_queue.wait(lock);
-                myPriority.readers_waiting--;
+            while (_lockOwned || _totalCurrentReaders == _max_threads || _find_first_priority(priority) < priority ){ 
+                myPriority.threads_waiting++;
+                myPriority.thread_queue.wait(lock);
+                myPriority.threads_waiting--;
             }
 
             _totalCurrentReaders++;
@@ -103,7 +99,7 @@ namespace PrioSync{// the name has yet to be chosen
 
             std::lock_guard<std::mutex> lock(_internalMtx);
             auto max_p = _find_first_priority(priority);
-            if (_lockOwned || _totalCurrentReaders == _Max_readers || max_p < priority)
+            if (_lockOwned || _totalCurrentReaders == _max_threads || max_p < priority)
                 return false;
             _totalCurrentReaders++;
             return true;
@@ -119,22 +115,21 @@ namespace PrioSync{// the name has yet to be chosen
             }
 
             if (p != _max_priority){
-                _priorities[p].writer_queue.notify_one();
-                _priorities[p].reader_queue.notify_all();
+                _priorities[p].thread_queue.notify_all();
             }
 
         }
 
         private:
 
-        Reader_cnt_t _totalCurrentReaders{};
+        Thread_cnt_t _totalCurrentReaders{};
         std::mutex _internalMtx;
         std::array<threadPriority, N> _priorities;
         bool _lockOwned{};
 
         Priority_t _find_first_priority(Priority_t priority = _max_priority){
             for (Priority_t i = 0; i < ((priority == _max_priority) ? N : priority); i++){
-                if (_priorities[i].writers_waiting > 0 || _priorities[i].readers_waiting > 0)
+                if (_priorities[i].threads_waiting > 0)
                     return i;
             }
             return _max_priority;
