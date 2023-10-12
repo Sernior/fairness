cd1e0de2afd6265dca72a7551e1dcd1af15e1a02
TorJack <124510313+SMartorana@users.noreply.github.com>
2023-10-12 17:41:08 +0200

    fix config.hpp docs

--- include/boost/fairness/config.hpp
+++ include/boost/fairness/config.hpp
@@ -2,7 +2,7 @@
  * @file config.hpp
  * @author F. Abrignani (federignoli@hotmail.it)
  * @author S. Martorana (salvatoremartorana@hotmail.com)
- * @brief This file contains the implementation of the slim_spinlock_priority_mutex.
+ * @brief This file contains configurations about boost and 128bit cpu support. TODO
  * @version 0.1
  * @date 2023-08-19
  * 
@@ -14,8 +14,8 @@
 #define BOOST_FAIRNESS_CONFIG_HPP
 #ifdef BOOST_FOUND
 #include <boost/atomic.hpp>
-#endif
+#endif // BOOST_FOUND
 #if defined(BOOST_ATOMIC_INT128_LOCK_FREE) && BOOST_ATOMIC_INT128_LOCK_FREE > 0
 #define BOOST_FAIRNESS_HAS_DWCAS
-#endif
+#endif // defined(BOOST_ATOMIC_INT128_LOCK_FREE) && BOOST_ATOMIC_INT128_LOCK_FREE > 0
 #endif // BOOST_FAIRNESS_CONFIG_HPP
