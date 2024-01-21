aac9744e24ba1f33cd66c1cc02fcd6f7838b7a4e
Salvatore Martorana <124510313+SMartorana@users.noreply.github.com>
2024-01-21 23:06:06 +0100

    update gh-pages

--- cmake/DeterministicConcurrency.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-  dc
-  GIT_REPOSITORY https://github.com/Sernior/deterministic-concurrency.git
-  GIT_TAG v1.4.5
-)
-
-FetchContent_MakeAvailable(dc)
