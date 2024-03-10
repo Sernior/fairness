27c4b3b840d84e5ec9b8e628dd32ce132ad7fabb
TorJack <salvatoremartorana@hotmail.com>
2024-03-10 22:05:38 +0100

    Add pipeline benchmarks

---
+++ benchmarks/priority_mutex_benchmark_pipeline_benchmark_audio.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file priority_mutex_benchmark_pipeline_benchmark_audio.cpp
+ * @author S. Martorana (salvatoremartorana@hotmail.com)
+ * @author F. Abrignani (federignoli@hotmail.it)
+ * @brief Alias priority_mutex_benchmark_pipeline_benchmark_audio.
+ * @version 0.1
+ * @date 2024-03-05
+ * @private
+ * @copyright Copyright (c) 2023 Federico Abrignani (federignoli@hotmail.it).
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt).
+ * 
+ */
+ 
+//#define BOOST_FAIRNESS_USE_TATAS
+//#define BOOST_FAIRNESS_SPINWAIT_SPINS 100
+//#define BOOST_FAIRNESS_SPINWAIT_SPINS_RELAXED 0
+//#define BOOST_FAIRNESS_GETREQUEST_SPINS 1
+//#define BOOST_FAIRNESS_GETREQUEST_SPINS_RELAXED 0
+//#define BOOST_FAIRNESS_USE_EXPERIMENTAL_WAIT_NOTIFY
+//#define BOOST_FAIRNESS_WAIT_SPINS 10000
+//#define BOOST_FAIRNESS_WAIT_SPINS_RELAXED 10000
+//#define BOOST_FAIRNESS_MAX_PQNODES 4
+
+#include <benchmark/benchmark.h>
+#include "pipeline_benchmark.hpp"
+
+BENCHMARK(priority_mutex_benchmark::pipeline_benchmark_audio)->Threads(BENCHMARK_THREADS);
+
+BENCHMARK_MAIN();
--- generate_reports.sh
+++ generate_reports.sh
@@ -13,6 +13,35 @@ record_performance() {
     sudo perf c2c record -o "reports/${threads}threads/spinlock_priority_mutex_benchmark_${threads}_c2c.data" ./build/benchmarks/spinlock_priority_mutex_benchmark_${threads}
     sudo perf record -o "reports/${threads}threads/standard_mutex_benchmark_${threads}.data" ./build/benchmarks/standard_mutex_benchmark_${threads}
     sudo perf c2c record -o "reports/${threads}threads/standard_mutex_benchmark_${threads}_c2c.data" ./build/benchmarks/standard_mutex_benchmark_${threads}
+
+    sudo perf record -o "reports/${threads}threads/priority_mutex_benchmark_pipeline_benchmark_long_${threads}.data" ./build/benchmarks/priority_mutex_benchmark_pipeline_benchmark_long_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/priority_mutex_benchmark_pipeline_benchmark_long_${threads}_c2c.data" ./build/benchmarks/priority_mutex_benchmark_pipeline_benchmark_long_${threads}
+    sudo perf record -o "reports/${threads}threads/spinlock_priority_mutex_benchmark_pipeline_benchmark_long_${threads}.data" ./build/benchmarks/spinlock_priority_mutex_benchmark_pipeline_benchmark_long_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/spinlock_priority_mutex_benchmark_pipeline_benchmark_long_${threads}_c2c.data" ./build/benchmarks/spinlock_priority_mutex_benchmark_pipeline_benchmark_long_${threads}
+    sudo perf record -o "reports/${threads}threads/standard_mutex_benchmark_pipeline_benchmark_long_${threads}.data" ./build/benchmarks/standard_mutex_benchmark_pipeline_benchmark_long_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/standard_mutex_benchmark_pipeline_benchmark_long_${threads}_c2c.data" ./build/benchmarks/standard_mutex_benchmark_pipeline_benchmark_long_${threads}
+
+    sudo perf record -o "reports/${threads}threads/priority_mutex_benchmark_pipeline_benchmark_gaming_${threads}.data" ./build/benchmarks/priority_mutex_benchmark_pipeline_benchmark_gaming_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/priority_mutex_benchmark_pipeline_benchmark_gaming_${threads}_c2c.data" ./build/benchmarks/priority_mutex_benchmark_pipeline_benchmark_gaming_${threads}
+    sudo perf record -o "reports/${threads}threads/spinlock_priority_mutex_benchmark_pipeline_benchmark_gaming_${threads}.data" ./build/benchmarks/spinlock_priority_mutex_benchmark_pipeline_benchmark_gaming_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/spinlock_priority_mutex_benchmark_pipeline_benchmark_gaming_${threads}_c2c.data" ./build/benchmarks/spinlock_priority_mutex_benchmark_pipeline_benchmark_gaming_${threads}
+    sudo perf record -o "reports/${threads}threads/standard_mutex_benchmark_pipeline_benchmark_gaming_${threads}.data" ./build/benchmarks/standard_mutex_benchmark_pipeline_benchmark_gaming_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/standard_mutex_benchmark_pipeline_benchmark_gaming_${threads}_c2c.data" ./build/benchmarks/standard_mutex_benchmark_pipeline_benchmark_gaming_${threads}
+
+    sudo perf record -o "reports/${threads}threads/priority_mutex_benchmark_pipeline_benchmark_audio_${threads}.data" ./build/benchmarks/priority_mutex_benchmark_pipeline_benchmark_audio_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/priority_mutex_benchmark_pipeline_benchmark_audio_${threads}_c2c.data" ./build/benchmarks/priority_mutex_benchmark_pipeline_benchmark_audio_${threads}
+    sudo perf record -o "reports/${threads}threads/spinlock_priority_mutex_benchmark_pipeline_benchmark_audio_${threads}.data" ./build/benchmarks/spinlock_priority_mutex_benchmark_pipeline_benchmark_audio_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/spinlock_priority_mutex_benchmark_pipeline_benchmark_audio_${threads}_c2c.data" ./build/benchmarks/spinlock_priority_mutex_benchmark_pipeline_benchmark_audio_${threads}
+    sudo perf record -o "reports/${threads}threads/standard_mutex_benchmark_pipeline_benchmark_audio_${threads}.data" ./build/benchmarks/standard_mutex_benchmark_pipeline_benchmark_audio_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/standard_mutex_benchmark_pipeline_benchmark_audio_${threads}_c2c.data" ./build/benchmarks/standard_mutex_benchmark_pipeline_benchmark_audio_${threads}
+
+    sudo perf record -o "reports/${threads}threads/priority_mutex_benchmark_pipeline_benchmark_fast_${threads}.data" ./build/benchmarks/priority_mutex_benchmark_pipeline_benchmark_fast_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/priority_mutex_benchmark_pipeline_benchmark_fast_${threads}_c2c.data" ./build/benchmarks/priority_mutex_benchmark_pipeline_benchmark_fast_${threads}
+    sudo perf record -o "reports/${threads}threads/spinlock_priority_mutex_benchmark_pipeline_benchmark_fast_${threads}.data" ./build/benchmarks/spinlock_priority_mutex_benchmark_pipeline_benchmark_fast_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/spinlock_priority_mutex_benchmark_pipeline_benchmark_fast_${threads}_c2c.data" ./build/benchmarks/spinlock_priority_mutex_benchmark_pipeline_benchmark_fast_${threads}
+    sudo perf record -o "reports/${threads}threads/standard_mutex_benchmark_pipeline_benchmark_fast_${threads}.data" ./build/benchmarks/standard_mutex_benchmark_pipeline_benchmark_fast_${threads}
+    sudo perf c2c record -o "reports/${threads}threads/standard_mutex_benchmark_pipeline_benchmark_fast_${threads}_c2c.data" ./build/benchmarks/standard_mutex_benchmark_pipeline_benchmark_fast_${threads}
+
 }
 
 record_performance 8
